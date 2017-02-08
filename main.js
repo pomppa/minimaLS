b1b73de4a809655b3e499f94c0f0af2ac71e4598
Tuomas Änäkkälä <tuomas.anakkala@icloud.com>
2017-02-08 13:22:54 +0200

    12-hour support and beautify

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 $(document).ready(function() {
-	updateClock();
-	setInterval(updateClock, 1000);
+    updateClock();
+    setInterval(updateClock, 1000);
 });
 
 function updateClock() {
-	var dateObj = new Date();
-  var hour = dateObj.getHours();
-  var minutes = dateObj.getMinutes();
+    var dateObj = new Date();
+    var hour = dateObj.getHours();
+    var minutes = dateObj.getMinutes();
 
-	$('.time').text(formatClock(hour) + '' + formatClock(minutes));
+    /*  12-hour
+		$('.time').text(formatClock(convertTwelveHour(hour)) + '' + formatClock(minutes)); */
+
+    /* 24-hour */
+    $('.time').text(formatClock(hour) + '' + formatClock(minutes));
 }
 
 function formatClock(time) {
-	if (time < 10) {
-		dateFormatted = '0' + time;
-		return dateFormatted;
-	} else {
-		return time;
-	}
+    if (time < 10) {
+        dateFormatted = '0' + time;
+        return dateFormatted;
+    } else {
+        return time;
+    }
+}
+
+function convertTwelveHour(time) {
+    if (time > 12) {
+        time = time - 12;
+        return time;
+    } else if (time == 0) {
+        time = 12;
+        return time;
+    } else {
+        return time;
+    }
 }
